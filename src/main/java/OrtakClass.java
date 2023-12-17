a1d9083ad428b27152c492e613ce26e9b5be6717
burcugocmen <Burcubaybars127@gmail.com>
2023-12-17 11:16:58 +0300

    first commit

---
+++ src/main/java/OrtakClass.java
@@ -0,0 +1,15 @@
+public class OrtakClass {
+    public static void main(String[] args) {
+        System.out.println("q1");
+
+
+    }
+}
+/*
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/burcugocmen/T127_GitHub.git
+git push -u origin main
+ */
